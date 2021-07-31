eb1126e96aeaf1d9d7402de16b107a8bdeecfccd
WengWeng <mr.houzhendong@gmail.com>
2021-07-31 13:44:25 +0800

    9.Metal

--- src/InOneWeekend/vec3.h
+++ src/InOneWeekend/vec3.h
@@ -64,14 +64,20 @@ public:
         return vec3(e[0] / n, e[1] / n, e[2] / n);
     }
 
-    inline static vec3 random()
+    static vec3 random()
     {
         return vec3(random_double(), random_double(), random_double());
     }
 
-    inline static vec3 random(double min, double max)
+    static vec3 random(double min, double max)
     {
         return vec3(random_double(min, max), random_double(min, max), random_double(min, max));
+    }
+
+    bool near_zero() const
+    {
+        const auto s = 1e-8;
+        return (fabs(e[0]) < s && fabs(e[1]) < s && fabs(e[2]) < s);
     }
 };
 
@@ -127,7 +133,7 @@ inline vec3 cross(const vec3 &u, const vec3 &v)
                 u.e[0] * v.e[1] - u.e[1] * v.e[0]);
 }
 
-vec3 random_in_unit_sphere()
+inline vec3 random_in_unit_sphere()
 {
     while (true)
     {
@@ -136,6 +142,25 @@ vec3 random_in_unit_sphere()
             continue;
         return p;
     }
+}
+
+inline vec3 random_unit_vector()
+{
+    return random_in_unit_sphere().normalized();
+}
+
+inline vec3 random_in_hemisphere(const vec3 &normal)
+{
+    vec3 in_unit_sphere = random_in_unit_sphere();
+    if (dot(in_unit_sphere, normal) > 0.0)
+        return in_unit_sphere;
+    else
+        return -in_unit_sphere;
+}
+
+inline vec3 reflect(const vec3 &v, const vec3 &n)
+{
+    return v - 2 * dot(v, n) * n;
 }
 
 #endif
