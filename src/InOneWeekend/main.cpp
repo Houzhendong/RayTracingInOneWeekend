d1a58bdfb9b6612c36510528971b2224d7d368f9
WengWeng <mr.houzhendong@gmail.com>
2021-08-04 21:53:04 +0800

    11.Positionable Camera

--- src/InOneWeekend/main.cpp
+++ src/InOneWeekend/main.cpp
@@ -51,7 +51,7 @@ int main()
     const int max_depth = 50;
 
     // File stream
-    std::string file = "Image12.ppm";
+    std::string file = "Image19.ppm";
     std::fstream filestream(file, std::ios::in | std::ios::binary | std::ios::app);
 
     if (!filestream.is_open())
@@ -61,17 +61,18 @@ int main()
     hittable_list world;
 
     auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
-    auto material_center = make_shared<lambertian>(color(0.7, 0.3, 0.3));
-    auto material_left = make_shared<metal>(color(0.8, 0.8, 0.8), 0.3);
-    auto material_right = make_shared<metal>(color(0.8, 0.6, 0.2), 1.0);
+    auto material_center = make_shared<lambertian>(color(0.1, 0.2, 0.5));
+    auto material_left = make_shared<dielectric>(1.5);
+    auto material_right = make_shared<metal>(color(0.8, 0.6, 0.2), 0.0);
 
     world.add(make_shared<sphere>(point3(0.0, -100.5, -1.0), 100.0, material_ground));
     world.add(make_shared<sphere>(point3(0.0, 0.0, -1.0), 0.5, material_center));
     world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), 0.5, material_left));
+    world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), -0.45, material_left));
     world.add(make_shared<sphere>(point3(1.0, 0.0, -1.0), 0.5, material_right));
 
     // Camera
-    camera cam;
+    camera cam(point3(-2, 2, 1), vec3(0, 0, -1), vec3(0, 1, 0), 20, aspect_ratio);
 
     // Render
     filestream << "P3\n"
