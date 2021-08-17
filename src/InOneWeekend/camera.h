bc34b864e9b3ec1e748075721d943f9bea3f94d6
WengWeng <mr.houzhendong@gmail.com>
2021-08-17 20:58:00 +0800

    Final Render

--- src/InOneWeekend/camera.h
+++ src/InOneWeekend/camera.h
@@ -10,26 +10,32 @@ public:
            vec3 lookat,
            vec3 up_direction,
            double vfov,
-           double aspect_ratio)
+           double aperture,
+           double aspect_ratio,
+           double focus_dist)
     {
         auto theta = degrees_to_radians(vfov);
         auto h = tan(theta / 2);
         auto viewport_height = 2.0 * h;
         auto viewport_width = aspect_ratio * viewport_height;
 
-        auto w = (position - lookat).normalized();
-        auto u = cross(up_direction, w).normalized();
-        auto v = cross(w, u);
+        w = (position - lookat).normalized();
+        u = cross(up_direction, w).normalized();
+        v = cross(w, u);
 
         origin = position;
-        horizontal = viewport_width * u;
-        vertical = viewport_height * v;
-        lower_left_corner = origin - horizontal / 2 - vertical / 2 - w;
+        horizontal = focus_dist * viewport_width * u;
+        vertical = focus_dist * viewport_height * v;
+        lower_left_corner = origin - horizontal / 2 - vertical / 2 - w * focus_dist;
+
+        lens_radius = aperture / 2;
     }
 
     ray get_ray(double s, double t) const
     {
-        return ray(origin, lower_left_corner + s * horizontal + t * vertical - origin);
+        vec3 rd = lens_radius * random_in_unit_disk();
+        vec3 offset = u * rd.x() + v * rd.y();
+        return ray(origin + offset, lower_left_corner + s * horizontal + t * vertical - origin - offset);
     }
 
 private:
@@ -37,6 +43,8 @@ private:
     point3 lower_left_corner;
     vec3 horizontal;
     vec3 vertical;
+    vec3 u, v, w;
+    double lens_radius;
 };
 
 #endif
--- src/InOneWeekend/main.cpp
+++ src/InOneWeekend/main.cpp
@@ -8,6 +8,58 @@
 #include <iostream>
 #include <fstream>
 
+hittable_list random_scene()
+{
+    hittable_list world;
+
+    auto ground_material = make_shared<lambertian>(color(0.5, 0.5, 0.5));
+    world.add(make_shared<sphere>(point3(0, -1000, 0), 1000, ground_material));
+
+    for (int a = -11; a < 11; a++)
+    {
+        for (int b = -11; b < 11; b++)
+        {
+            auto choose_mat = random_double();
+            point3 center(a + 0.9 * random_double(), 0.2, b + 0.9 * random_double());
+
+            if ((center - point3(4, 0.2, 0)).length() > 0.9)
+            {
+                shared_ptr<material> sphere_material;
+                if (choose_mat < 0.8)
+                {
+                    // diffuse
+                    auto albedo = color::random() * color::random();
+                    sphere_material = make_shared<lambertian>(albedo);
+                }
+                else if (choose_mat < 0.95)
+                {
+                    // metal
+                    auto albedo = color::random(0.5, 1);
+                    auto fuzz = random_double(0, 0.5);
+                    sphere_material = make_shared<metal>(albedo, fuzz);
+                }
+                else
+                {
+                    // glass
+                    sphere_material = make_shared<dielectric>(1.5);
+                }
+                world.add(make_shared<sphere>(center, 0.2, sphere_material));
+            }
+        }
+    }
+
+    auto material1 = make_shared<dielectric>(1.5);
+    world.add(make_shared<sphere>(point3(0, 1, 0), 1.0, material1));
+
+    auto material2 = make_shared<lambertian>(color(0.4, 0.2, 0.1));
+    world.add(make_shared<sphere>(point3(-4, 1, 0), 1.0, material2));
+
+    auto material3 = make_shared<metal>(color(0.7, 0.6, 0.5), 0.0);
+    world.add(make_shared<sphere>(point3(4, 1, 0), 1.0, material3));
+
+    return world;
+}
+
 double hit_sphere(const point3 &center, double radius, const ray &r)
 {
     vec3 oc = r.origin() - center;
@@ -44,22 +96,23 @@ color ray_color(const ray &r, const hittable &world, int depth)
 int main()
 {
     // Image
-    const auto aspect_ratio = 16.0 / 9.0;
-    const int image_width = 400;
+    const auto aspect_ratio = 3.0 / 2.0;
+    const int image_width = 1200;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
-    const int samples_per_pixel = 100;
+    const int samples_per_pixel = 500;
     const int max_depth = 50;
 
     // File stream
-    std::string file = "Image19.ppm";
+    std::string file = "FinalImage.ppm";
     std::fstream filestream(file, std::ios::in | std::ios::binary | std::ios::app);
 
     if (!filestream.is_open())
         std::cout << "Open file failed." << std::endl;
 
     // World
-    hittable_list world;
+    hittable_list world = random_scene();
 
+    /*
     auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
     auto material_center = make_shared<lambertian>(color(0.1, 0.2, 0.5));
     auto material_left = make_shared<dielectric>(1.5);
@@ -70,9 +123,15 @@ int main()
     world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), 0.5, material_left));
     world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), -0.45, material_left));
     world.add(make_shared<sphere>(point3(1.0, 0.0, -1.0), 0.5, material_right));
+    */
 
     // Camera
-    camera cam(point3(-2, 2, 1), vec3(0, 0, -1), vec3(0, 1, 0), 20, aspect_ratio);
+    point3 position(13, 2, 3);
+    point3 lookat(0, 0, 0);
+    point3 up_direction(0, 1, 0);
+    auto dist_to_focus = 10.0;
+    auto aperture = 0.1;
+    camera cam(position, lookat, up_direction, 20, aperture, aspect_ratio, dist_to_focus);
 
     // Render
     filestream << "P3\n"
